016c07b14436f77e44462ba065439df527d70378
lorianchester <lec128@georgetown.edu>
2022-01-30 21:48:21 -0500

    Update game.py

--- game.py
+++ game.py
@@ -7,24 +7,67 @@
 #
 
 
-
+print("Welcome 'Player One' to my Rock-Paper-Scissors game!")
 
 # ASK FOR USER INPUT
 user_choice = input("Please choose one of: 'rock', 'paper', 'scissors': ")
 
+#convert user input to all lowercase
+user_choice.lower()
+
+# VALIDATIONS
+if user_choice != "rock" and user_choice != "paper" and user_choice != "scissors":
+    print("Sorry! You must choose 'rock', 'paper', or 'scissors'.")
+    exit()
+
 print("User chose: ", user_choice)
 
 
-
-# VALIDATIONS
-
-
-
 # COMPUTER CHOICE
-
+import random
+selection = ['rock', 'paper', 'scissors']
+computer_choice = random.choice(selection)
 
 
 # DETERMINE THE WINNER
+#scissors beats rock
+if user_choice =='rock' and computer_choice == 'scissors':
+    winner = "Player One"
+if computer_choice == 'rock' and user_choice == 'scissors':
+    winner = "computer"
 
+#paper beats rock
+if user_choice == 'paper' and computer_choice == 'rock':
+    winner = "Player One"
+if computer_choice == 'paper' and user_choice == 'rock':
+    winner = "computer"
+
+#scissors beats paper
+if user_choice == 'scissors' and computer_choice == 'paper':
+    winner = "Player One"
+if computer_choice == 'scissors' and user_choice == 'paper':
+    winner = "computer"
+
+#rock v rock, paper v paper, and scissors v paper result in a "tie"
+if user_choice == 'rock' and computer_choice == 'rock':
+    winner = "tie"
+if user_choice == 'paper' and computer_choice == 'paper':
+    winner = "tie"
+if user_choice == 'scissors' and computer_choice == 'scissors':
+    winner = "tie"
 
 # FINAL RESULTS
+
+#show user choice
+print("Player One chose: ", user_choice)
+#show computer choice
+print("The computer chose": computer_choice)
+
+if winner == "Player One":
+    print("Player One wins!")
+elif winner == "computer":
+    print("Sorry! The computer won.")
+else:
+    print("It's a tie!")
+
+print("Thanks for playing! Goodbye!")
