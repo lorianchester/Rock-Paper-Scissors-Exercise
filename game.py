1ff845ffabf38542d1e83dbc2aaa1e6896fcdb64
lorianchester <lec128@georgetown.edu>
2022-02-02 17:44:17 -0500

    finished draft

--- game.py
+++ game.py
@@ -6,8 +6,12 @@
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
 
+#allow user to change their name
+import os
 
-print("Welcome 'Player One' to my Rock-Paper-Scissors game!")
+player_name = os.getenv("PLAYER_NAME", default="Player One")
+
+print("Welcome ", player_name, " to my Rock-Paper-Scissors game!")
 
 # ASK FOR USER INPUT
 user_choice = input("Please choose one of: 'rock', 'paper', 'scissors': ")
@@ -18,7 +22,7 @@ if user_choice != "rock" and user_choice != "paper" and user_choice != "scissors
     print("Sorry! You must choose 'rock', 'paper', or 'scissors'.")
     exit()
 
-print("User chose: ", user_choice)
+print(player_name, "chose: ", user_choice)
 
 # COMPUTER CHOICE
 from random import choice
@@ -34,21 +38,21 @@ print("Computer chose: ", computer_choice)
 #scissors beats rock
 
 if user_choice == "rock" and computer_choice == "scissors":
-    result = "Player One wins!"
+    result = player_name, " wins!"
 
 elif computer_choice == "rock" and user_choice == "scissors":
     result = "Sorry! The computer won."
 
 #paper beats rock
 elif user_choice == "paper" and computer_choice == "rock":
-    result = "Player One wins!"
+    result = player_name, " wins!"
 
 elif computer_choice == "paper" and user_choice == "rock":
     result = "Sorry! The computer won."
 
 #scissors beats paper
 elif user_choice == "scissors" and computer_choice == "paper":
-    result = "Player One wins!"
+    result = player_name, " wins!"
 
 elif computer_choice == "scissors" and user_choice == "paper":
     result = "Sorry! The computer won."
