1247293153d7cbdfb45506208685986a871cb82c
lorianchester <lec128@georgetown.edu>
2022-02-03 15:17:37 -0500

    Update game.py

--- game.py
+++ game.py
@@ -38,21 +38,21 @@ print("Computer chose: ", computer_choice)
 #scissors beats rock
 
 if user_choice == "rock" and computer_choice == "scissors":
-    result = player_name, " wins!"
+    result = player_name + " wins!"
 
 elif computer_choice == "rock" and user_choice == "scissors":
     result = "Sorry! The computer won."
 
 #paper beats rock
 elif user_choice == "paper" and computer_choice == "rock":
-    result = player_name, " wins!"
+    result = player_name + " wins!"
 
 elif computer_choice == "paper" and user_choice == "rock":
     result = "Sorry! The computer won."
 
 #scissors beats paper
 elif user_choice == "scissors" and computer_choice == "paper":
-    result = player_name, " wins!"
+    result = player_name + " wins!"
 
 elif computer_choice == "scissors" and user_choice == "paper":
     result = "Sorry! The computer won."
